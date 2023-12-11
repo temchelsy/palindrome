2cf99c1ea3d51ce1c75c572715b675a52a6145d4
tem chelsy <temchelsy@gmail.com>
2023-12-11 09:35:57 +0100

    3 modification of my palindrome

--- chelsy.js
+++ chelsy.js
@@ -1,5 +1,5 @@
 function palindromeTest (str) {
-    let len = str.length - 1;
+  let len = str.length - 1
   for (let i = 0; i < str.length /2; i++) {
     if (str[i] !== str[len]) {
       return false
@@ -7,20 +7,17 @@ function palindromeTest (str) {
     len--
   }
   return true
-
 }
 console.log(palindromeTest('babb'))
 
 function checkPalindrome () {
-    
   const pali = document.getElementById('text-area').value
   const isPalindrome = palindromeTest(pali)
   const ema = document.getElementById('result')
-  
   if (isPalindrome) {
     ema.innerHTML = 'the text entered is a palinndrome'
   } else {
     ema.innerHTML = 'sorry,it is not a palindrome'
     }
 }
-checkPalindrome()
+  checkPalindrome()
