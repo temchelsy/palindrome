bd0cf1a78071e6a8a2e1108fc7a35f4fccae37aa
tem chelsy <temchelsy@gmail.com>
2023-12-11 08:48:46 +0100

    second modifivation of my palindrome check

--- chelsy.js
+++ chelsy.js
@@ -1,25 +1,26 @@
-function palindromeTest(str) {
+function palindromeTest (str) {
     let len = str.length - 1;
-    for (let i = 0; i < str.length/2; i++) {
-        if (str[i] !== str[len]) {
-            return false
-        }
-        len--
+  for (let i = 0; i < str.length /2; i++) {
+    if (str[i] !== str[len]) {
+      return false
     }
-    return true
+    len--
+  }
+  return true
 
 }
 console.log(palindromeTest('babb'))
 
-function checkPalindrome() {
-    const pali = document.getElementById('text-area').value
-    const isPalindrome = palindromeTest(pali)
-    const ema = document.getElementById('result')
-
-    if (isPalindrome) {
-        ema.innerHTML = 'the text entered is a palinndrome'
-    } else {
-        ema.innerHTML = 'sorry,it is not a palindrome'
+function checkPalindrome () {
+    
+  const pali = document.getElementById('text-area').value
+  const isPalindrome = palindromeTest(pali)
+  const ema = document.getElementById('result')
+  
+  if (isPalindrome) {
+    ema.innerHTML = 'the text entered is a palinndrome'
+  } else {
+    ema.innerHTML = 'sorry,it is not a palindrome'
     }
 }
-// checkPalindrome()
+checkPalindrome()
